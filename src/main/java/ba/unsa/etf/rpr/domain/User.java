b7e99a68f7afabefc10bc4f89e0523f1b114af64
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-10 17:00:25 +0100

    Changed constructor in class User and added getter and setter for new attributes

--- src/main/java/ba/unsa/etf/rpr/domain/User.java
+++ src/main/java/ba/unsa/etf/rpr/domain/User.java
@@ -9,8 +9,11 @@ public class User implements Idable {
     private String username;
     private String password;
 
-    public User(int id, String username, String password) {
+    public User(int id, String firstName, String lastName, String email, String username, String password) {
         this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
         this.username = username;
         this.password = password;
     }
@@ -41,5 +44,29 @@ public class User implements Idable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
