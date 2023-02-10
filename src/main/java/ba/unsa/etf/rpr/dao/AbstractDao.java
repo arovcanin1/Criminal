7e5e55f897f7f73339aa833410b28f78a331aac9
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-10 18:24:41 +0100

    Error - Target folder

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -1,8 +1,47 @@
 package ba.unsa.etf.rpr.dao;
 
+
 import ba.unsa.etf.rpr.domain.Idable;
+import ba.unsa.etf.rpr.exceptions.CriminalRecordsException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+
 
 public abstract class AbstractDao<T extends Idable> implements Dao<T> {
+
+   private Connection connection = null;
+   private String table;
+
+   public AbstractDao(String table) {
+      this.table = table;
+      if (connection == null) {
+         try {
+            Properties p = new Properties();
+            p.load(ClassLoader.getSystemResource("application.properties").openStream());
+            String url = p.getProperty("db.url");
+            String username = p.getProperty("db.username");
+            String password = p.getProperty("db.password");
+            this.connection = DriverManager.getConnection(url, username, password);
+         }
+         catch(Exception e) {
+            System.out.println("Connection to database failed!");
+            e.printStackTrace();
+         }
+      }
+   }
+
+   public Connection getConnection() {
+      return this.connection;
+   }
+
+   public T getById(int id) throws CriminalRecordsException {
+      return null;
+   }
+
+
 
 
 }
--- src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
@@ -5,6 +5,10 @@ import ba.unsa.etf.rpr.exceptions.CriminalRecordsException;
 
 public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao {
 
+    public UserDaoSQLImpl() {
+        super("users");
+    }
+
 
     @Override
     public User getById(int id) throws CriminalRecordsException {
