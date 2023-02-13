3029fb128ddf97e5bb9f2b1326ead3c2b3db6bf0
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-13 22:40:19 +0100

    New controllers for Criminals added

--- src/main/java/ba/unsa/etf/rpr/controllers/EmployeeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/EmployeeController.java
@@ -1,10 +1,22 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.domain.Criminal;
 import ba.unsa.etf.rpr.domain.Employee;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class EmployeeController {
 
     private Employee employee;
+    public Button criminalsBtn;
 
     public EmployeeController() {
         employee = new Employee();
@@ -15,6 +27,23 @@ public class EmployeeController {
     }
 
     public void initialize() {
+
+    }
+
+    public void showAllCriminals(ActionEvent event) {
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/criminals.fxml"));
+            loader.setController(new CriminalController());
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("CR Criminals");
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
--- src/main/java/ba/unsa/etf/rpr/controllers/ExampleController.java
+++
@@ -1,9 +0,0 @@
-package ba.unsa.etf.rpr.controllers;
-
-import ba.unsa.etf.rpr.domain.Employee;
-
-public class ExampleController {
-
-
-
-}
