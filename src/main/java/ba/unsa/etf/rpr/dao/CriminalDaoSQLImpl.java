56e399021c03f551d476c032d0f5bb127c23bb53
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-13 22:19:59 +0100

    Method for mapping rs to Object, row2object, for Criminals

--- src/main/java/ba/unsa/etf/rpr/dao/CriminalDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CriminalDaoSQLImpl.java
@@ -1,10 +1,13 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Criminal;
+import ba.unsa.etf.rpr.domain.Gender;
 import ba.unsa.etf.rpr.exceptions.CriminalRecordsException;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class CriminalDaoSQLImpl extends AbstractDao<Criminal> {
 
@@ -15,7 +18,18 @@ public class CriminalDaoSQLImpl extends AbstractDao<Criminal> {
     }
 
     public Criminal row2object(ResultSet rs) throws CriminalRecordsException {
-        return null;
+        try {
+            Criminal criminal = new Criminal();
+            criminal.setId(rs.getInt("id"));
+            criminal.setFirstName(rs.getString("firstName"));
+            criminal.setLastName(rs.getString("lastName"));
+            criminal.setJmbg(rs.getString("jmbg"));
+            criminal.setBirthDate(rs.getDate("date"));
+            criminal.setGender(Gender.valueOf(rs.getString("gender")));
+            return criminal;
+        } catch (SQLException e) {
+            throw new CriminalRecordsException(e.getMessage());
+        }
     }
 
     public Map<String, Object> object2row (Criminal object) {
