9d7935ec358b09b8d7fa2e14028ba9c412d63fee
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-15 00:48:06 +0100

    JavaDoc for class Employee

--- src/main/java/ba/unsa/etf/rpr/domain/Employee.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Employee.java
@@ -2,8 +2,15 @@ package ba.unsa.etf.rpr.domain;
 
 import java.util.Objects;
 
+/**
+ * Domain JavaBean class for Employee with all attributes such as id, firstName, lastName, email, username and password
+ * In this class there are also constructor, getters and setters for each attribute
+ */
 public class Employee implements Idable {
 
+    /**
+     * Atributes
+     */
     private int id;
     private String firstName;
     private String lastName;
@@ -11,6 +18,15 @@ public class Employee implements Idable {
     private String username;
     private String password;
 
+    /**
+     * Constructor with all parameters
+     * @param id
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param username
+     * @param password
+     */
     public Employee(int id, String firstName, String lastName, String email, String username, String password) {
         this.id = id;
         this.firstName = firstName;
@@ -20,58 +36,114 @@ public class Employee implements Idable {
         this.password = password;
     }
 
+    /**
+     * Constructor without parameters
+     */
     public Employee() {}
 
+    /**
+     * Getter for Id
+     * @return
+     */
     @Override
     public int getId() {
         return id;
     }
 
+    /**
+     * Setter for id
+     * @param id
+     */
     @Override
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Getter for username
+     * @return
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Setter for username
+     * @param username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Getter for password
+     * @return
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Setter for password
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Getter for attribute firstName
+     * @return
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Setter for attribute firstName
+     * @param firstName
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Getter for attribute lastName
+     * @return
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Setter for attribute lastName
+     * @param lastName
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Getter for attribute email
+     * @return
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Setter for attribute email
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Method for comparing Employees by id, firstName, lastName, username and email
+     * @param obj
+     * @return
+     */
     @Override
     public boolean equals(Object obj){
         if (obj == this) return true;
@@ -83,7 +155,11 @@ public class Employee implements Idable {
                 Objects.equals(email, employee.email);
     }
 
-   @Override
+    /**
+     * Method for hashing attributes of employee
+     * @return
+     */
+    @Override
     public int hashCode() {
         return Objects.hash(id, firstName, lastName, username, email);
    }
