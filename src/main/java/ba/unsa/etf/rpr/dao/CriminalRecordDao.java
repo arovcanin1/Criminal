331123bd8a69feba5154d635318aa95ff335a7da
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-14 19:42:42 +0100

    Methods for criminal records added

---
+++ src/main/java/ba/unsa/etf/rpr/dao/CriminalRecordDao.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Criminal;
+import ba.unsa.etf.rpr.domain.CriminalRecord;
+
+public interface CriminalRecordDao extends Dao<CriminalRecord> {
+}
--- src/main/java/ba/unsa/etf/rpr/dao/CriminalRecordSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CriminalRecordSQLImpl.java
@@ -1,4 +1,26 @@
 package ba.unsa.etf.rpr.dao;
 
-public class CriminalRecordSQLImpl {
+import ba.unsa.etf.rpr.domain.Criminal;
+import ba.unsa.etf.rpr.domain.CriminalRecord;
+import ba.unsa.etf.rpr.exceptions.CriminalRecordsException;
+
+import java.sql.ResultSet;
+import java.util.Map;
+
+public class CriminalRecordSQLImpl extends AbstractDao<CriminalRecord> implements CriminalRecordDao {
+    public CriminalRecordSQLImpl() {
+        super("CriminalRecord");
+    }
+
+    public CriminalRecord getById(int id) throws CriminalRecordsException {
+        return null;
+    }
+
+    public CriminalRecord row2object(ResultSet rs) throws CriminalRecordsException {
+        return null;
+    }
+
+    public Map<String, Object> object2row(CriminalRecord object) {
+        return null;
+    }
 }
