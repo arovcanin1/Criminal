f1099fac97ec0d8272b84e5e732048ff14374ecc
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-14 19:49:37 +0100

    Method for getting criminal record by id implemented

--- src/main/java/ba/unsa/etf/rpr/dao/CriminalRecordSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CriminalRecordSQLImpl.java
@@ -4,7 +4,9 @@ import ba.unsa.etf.rpr.domain.Criminal;
 import ba.unsa.etf.rpr.domain.CriminalRecord;
 import ba.unsa.etf.rpr.exceptions.CriminalRecordsException;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
 
 public class CriminalRecordSQLImpl extends AbstractDao<CriminalRecord> implements CriminalRecordDao {
@@ -13,7 +15,22 @@ public class CriminalRecordSQLImpl extends AbstractDao<CriminalRecord> implement
     }
 
     public CriminalRecord getById(int id) throws CriminalRecordsException {
-        return null;
+        String query = "SELECT * FROM CriminalRecord WHERE id = ?";
+
+        try {
+            PreparedStatement statement = getConnection().prepareStatement(query);
+            statement.setInt(1, id);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                CriminalRecord result = row2object(rs);
+                rs.close();
+                return result;
+            } else {
+                throw  new CriminalRecordsException("Criminal record does not exist!");
+            }
+        } catch (SQLException e) {
+            throw new CriminalRecordsException(e.getMessage(), e);
+        }
     }
 
     public CriminalRecord row2object(ResultSet rs) throws CriminalRecordsException {
