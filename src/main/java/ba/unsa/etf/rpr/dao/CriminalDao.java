17031a62e29d425dd433de5e4b4c9b78a08dbbe2
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-15 09:16:10 +0100

    JavaDoc for CriminalDao interface

--- src/main/java/ba/unsa/etf/rpr/dao/CriminalDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CriminalDao.java
@@ -6,9 +6,23 @@ import ba.unsa.etf.rpr.exceptions.CriminalRecordsException;
 
 import java.util.List;
 
+/**
+ * Interface that extends Dao<T> and also has methods for CriminalDao
+ */
 public interface CriminalDao extends Dao<Criminal> {
 
+      /**
+       * Method that find all criminals
+       * @return list of all criminals
+       * @throws CriminalRecordsException
+       */
       public List<Criminal> allCriminals() throws CriminalRecordsException;
-      public Criminal getByJMBG(String jmbg) throws CriminalRecordsException;
 
+      /**
+       * Method that find all criminals based on jmbg
+       * @param jmbg
+       * @return
+       * @throws CriminalRecordsException
+       */
+      public Criminal getByJMBG(String jmbg) throws CriminalRecordsException;
 }
