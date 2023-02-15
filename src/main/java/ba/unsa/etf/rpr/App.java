db2c6d867b9f87ebabcd9a69107315f854dd89f0
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-15 10:05:55 +0100

    Registration for employee

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.business.EmployeeManager;
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Employee;
 import ba.unsa.etf.rpr.exceptions.CriminalRecordsException;
@@ -30,7 +31,7 @@ public class App {
                     break;
                 }
 
-                if (employee == null ) {
+                if (employee == null) {
                     System.out.println("Enter username again: ");
                     String repeatUsername;
                     Scanner repeatScaner = new Scanner(System.in);
@@ -44,11 +45,44 @@ public class App {
                     repeatUsername = repeatScaner.next();
                 }
 
-                showHome(employee.getId());
+                showEmployee(employee.getId());
             }
         }
 
+        if (scanner.next().equals("R")) {
+            String firstName;
+            String lastName;
+            String username;
+            String email;
+            String password;
+            String confirmPassword;
+            System.out.println("Enter name: ");
+            Scanner enterName = new Scanner(System.in);
+            firstName = enterName.next();
+            System.out.println("Enter surname: ");
+            Scanner enterSurname = new Scanner(System.in);
+            lastName = enterSurname.next();
+            Scanner enterUsername = new Scanner(System.in);
+            username = enterUsername.next();
+            Scanner enterEmail = new Scanner(System.in);
+            email = enterEmail.next();
+            Scanner enterPassword = new Scanner(System.in);
+            password = enterPassword.next();
+            Scanner enterPasswordAgain = new Scanner(System.in);
+            confirmPassword = enterPasswordAgain.next();
 
+            Employee e = new Employee();
+            e.setFirstName(firstName);
+            e.setLastName(lastName);
+            e.setUsername(username);
+            e.setPassword(password);
+            EmployeeManager.add(e, confirmPassword);
+            showEmployee(e.getId());
+        }
+    }
 
+    public static void showEmployee(int id)  {
+        System.out.println("You have following options: ");
+        System.out.println("");
     }
 }
