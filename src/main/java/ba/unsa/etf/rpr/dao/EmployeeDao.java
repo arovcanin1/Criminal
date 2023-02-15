a7460fd6c5e2eeb035d1cac949b194d6b7950a3f
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-15 09:17:59 +0100

    JavaDoc for EmployeeDao interface

--- src/main/java/ba/unsa/etf/rpr/dao/EmployeeDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/EmployeeDao.java
@@ -3,8 +3,16 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Employee;
 import ba.unsa.etf.rpr.exceptions.CriminalRecordsException;
 
+/**
+ * Interface that extends Dao<T> and also has methods only for EmployeeDao
+ */
 public interface EmployeeDao extends Dao<Employee> {
 
-
+    /**
+     * Method for getting Employee by username
+     * @param username
+     * @return
+     * @throws CriminalRecordsException
+     */
     Employee getByUsername(String username) throws CriminalRecordsException;
 }
