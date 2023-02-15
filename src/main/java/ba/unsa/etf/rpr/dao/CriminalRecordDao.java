ec2fd0c90e90d69a91a730c89d2db657ad6ab515
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-15 09:20:32 +0100

    JavaDoc for CriminalRecordDao interface

--- src/main/java/ba/unsa/etf/rpr/dao/CriminalRecordDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CriminalRecordDao.java
@@ -1,13 +1,28 @@
 package ba.unsa.etf.rpr.dao;
 
-import ba.unsa.etf.rpr.domain.Criminal;
 import ba.unsa.etf.rpr.domain.CriminalRecord;
 import ba.unsa.etf.rpr.exceptions.CriminalRecordsException;
 
 import java.util.List;
 
+/**
+ * Interface that extends Dao<T> and also has methods only for CriminalRecordDao
+ */
 public interface CriminalRecordDao extends Dao<CriminalRecord> {
 
+    /**
+     * Method for getting all records based on criminal id
+     * @param id
+     * @return list of all criminal records with specified id
+     * @throws CriminalRecordsException
+     */
     public List<CriminalRecord> getByIdNew(int id) throws CriminalRecordsException;
+
+    /**
+     * Method for getting all records based on code
+     * @param code
+     * @return criminal record with specified code
+     * @throws CriminalRecordsException
+     */
     public CriminalRecord getByCode(String code) throws CriminalRecordsException;
 }
