dbb9f479ca92b7655e286f02d7bd88aedaa47084
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 10:15:39 +0100

    test for adding criminal when some fields are null

--- src/test/java/ba/unsa/etf/rpr/business/CriminalManager.java
+++ src/test/java/ba/unsa/etf/rpr/business/CriminalManager.java
@@ -1,9 +1,14 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Criminal;
+import ba.unsa.etf.rpr.exceptions.CriminalRecordsException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CriminalManager {
 
@@ -16,5 +21,15 @@ public class CriminalManager {
         criminal.setLastName("");
         criminal.setJmbg("2705996175015");
         criminal.setBirthDate(LocalDate.of(1996,05,27));
+    }
+
+    @Test
+    public void exceptionTest() {
+        try {
+            DaoFactory.criminalsDao().add(criminal);
+        } catch (CriminalRecordsException e) {
+            assertEquals("All fields have to be filled!", e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
