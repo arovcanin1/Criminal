fd040ce0bf777a3539e641a48e581ed7d2d466fd
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 05:33:39 +0100

    added button for update employee data

--- src/main/java/ba/unsa/etf/rpr/controllers/ProfileController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ProfileController.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.domain.Employee;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -13,6 +14,8 @@ public class ProfileController {
     public TextField emailFld;
     public TextField usernameFld;
     public PasswordField passwordFld;
+
+    public Button updateBtn;
 
     public ProfileController(Employee employee) {
         this.employee = employee;
