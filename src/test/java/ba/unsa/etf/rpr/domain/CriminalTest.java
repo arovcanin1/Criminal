25e7af3b41d378b0bb6a44162d866cb5750bd145
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 09:15:25 +0100

    test for criminal added

--- src/test/java/ba/unsa/etf/rpr/domain/CriminalTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/CriminalTest.java
@@ -1,8 +1,11 @@
 package ba.unsa.etf.rpr.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CriminalTest {
 
@@ -10,9 +13,32 @@ public class CriminalTest {
 
     @BeforeEach
     public void setCriminal() {
+        LocalDate localDate = LocalDate.of(1996,05,27);
         criminal.setId(1);
         criminal.setFirstName("Criminal");
         criminal.setLastName("Criminal");
         criminal.setJmbg("2705996175015");
+        criminal.setBirthDate(localDate);
+    }
+
+    @Test
+    void getFirstName() {
+        assertEquals("Criminal", criminal.getFirstName());
+    }
+
+    @Test
+    void getLastName() {
+        assertEquals("Criminal", criminal.getLastName());
+    }
+
+    @Test
+    void getJmbg() {
+        assertEquals("2705996175015", criminal.getJmbg());
+    }
+
+    @Test
+    void toStringTest() {
+        String s = "First name: " + criminal.getFirstName() + "\nLast name: " + criminal.getLastName() + "\nJMBG: " + criminal.getJmbg() + "\nBirth date: " + criminal.getBirthDate();
+        assertEquals(s, criminal.toString());
     }
 }
