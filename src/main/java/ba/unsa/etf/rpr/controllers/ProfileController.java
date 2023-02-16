e5d65308047c2a8c31294138cbf7c38b8248d155
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 05:21:39 +0100

    labels and fileds filled with employee data

--- src/main/java/ba/unsa/etf/rpr/controllers/ProfileController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ProfileController.java
@@ -2,10 +2,17 @@ package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.domain.Employee;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class ProfileController {
     Employee employee = new Employee();
     public Label welcomeLabel;
+    public TextField firstNameFld;
+    public TextField lastNameFld;
+    public TextField emailFld;
+    public TextField usernameFld;
+    public PasswordField passwordFld;
 
     public ProfileController(Employee employee) {
         this.employee = employee;
@@ -14,5 +21,10 @@ public class ProfileController {
 
     public void initialize() {
         welcomeLabel.setText("Welcome, " + employee.getFirstName());
+        firstNameFld.setText(employee.getFirstName());
+        lastNameFld.setText(employee.getLastName());
+        emailFld.setText(employee.getEmail());
+        usernameFld.setText(employee.getUsername());
+        passwordFld.setText(employee.getPassword());
     }
 }
