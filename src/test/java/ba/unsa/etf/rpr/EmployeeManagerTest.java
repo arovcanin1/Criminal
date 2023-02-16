6441b87cd948a04062e664e600fcc782de37451b
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 09:56:48 +0100

    setting employee for testing

--- src/test/java/ba/unsa/etf/rpr/EmployeeManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/EmployeeManagerTest.java
@@ -1,4 +1,18 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.domain.Employee;
+import org.junit.jupiter.api.BeforeAll;
+
 public class EmployeeManagerTest {
+    Employee employee = new Employee();
+
+    @BeforeAll
+    public void setEmployee() {
+        employee.setId(1);
+        employee.setFirstName("Amila");
+        employee.setLastName("Rovcanin");
+        employee.setUsername("arovcanin1");
+        employee.setEmail("arovcanin1@etf.unsa.ba");
+        employee.setPassword("someBadPass");
+    }
 }
