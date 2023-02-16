a08eff5d91008733ef48c82ddc6cd78556f53eac
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 07:11:34 +0100

    javaDoc for DaoFactory class

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,21 +1,40 @@
 package ba.unsa.etf.rpr.dao;
 
-import ba.unsa.etf.rpr.domain.Criminal;
-import ba.unsa.etf.rpr.domain.CriminalRecord;
 
+/**
+ * DaoFactory class that make instances of dao classes
+ */
 public class DaoFactory {
 
+    /**
+     * Static methods for DaoFactory
+     */
     private static final EmployeeDao employeesDao = new EmployeeDaoSQLImpl();
     private static  final CriminalDao criminalsDao = new CriminalDaoSQLImpl();
-
     private static final CriminalRecordDao criminalRecordsDao = new CriminalRecordSQLImpl();
 
+    /**
+     * Constructor without parameters
+     */
     private DaoFactory() {}
 
+    /**
+     * Method for access EmployeeDao instance
+     * @return
+     */
     public static EmployeeDao employeesDao() {
         return employeesDao;
     }
+
+    /**
+     * Method for access CriminalDao instance
+     * @return
+     */
     public static CriminalDao criminalsDao() { return criminalsDao; }
 
+    /**
+     * Method for access CriminalRecordDao instance
+     * @return
+     */
     public static CriminalRecordDao criminalRecordsDao() {return criminalRecordsDao; }
 }
