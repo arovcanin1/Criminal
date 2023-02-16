3a41c5fd5be2202c8e8597d74c63bb0f2a298ccb
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 08:10:08 +0100

    restrictions for adding new record added

--- src/main/java/ba/unsa/etf/rpr/business/CriminalRecordManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/CriminalRecordManager.java
@@ -4,6 +4,10 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Criminal;
 import ba.unsa.etf.rpr.domain.CriminalRecord;
 import ba.unsa.etf.rpr.exceptions.CriminalRecordsException;
+import javafx.util.converter.LocalDateStringConverter;
+
+import java.time.LocalDate;
+import java.time.Period;
 
 /**
  * Class that is business layer for CriminalRecord
@@ -18,6 +22,10 @@ public class CriminalRecordManager {
      * @throws CriminalRecordsException
      */
     public static Criminal add(CriminalRecord record) throws CriminalRecordsException {
+        if (record.getDate().isAfter(LocalDate.now())) {
+            throw new CriminalRecordsException("Date is not correct");
+        }
+
         try {
             return DaoFactory.criminalRecordsDao().add(record).getCriminal();
         } catch (CriminalRecordsException e) {
