36185074721b0804058a6088ac47b98f743b50cc
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 09:25:11 +0100

    setting criminal and criminal record for testing

--- src/test/java/ba/unsa/etf/rpr/domain/CriminalRecordTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/CriminalRecordTest.java
@@ -1,4 +1,17 @@
 package ba.unsa.etf.rpr.domain;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CriminalRecordTest {
+    Criminal criminal = new Criminal(1, "Criminal", "Criminal", "2705996175015", LocalDate.of(1996,05,27));
+    CriminalRecord criminalRecord = new CriminalRecord(9, "someDescription", "Stockholm", LocalDate.of(2022,05,23), "someCode", criminal);
+
+    @Test
+    public void getCriminalTest() {
+        assertEquals(criminal, criminalRecord.getCriminal());
+    }
 }
