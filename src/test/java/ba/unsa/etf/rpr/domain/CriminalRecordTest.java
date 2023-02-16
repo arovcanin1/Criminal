8f139306374b915b1cb9b170687abba2e8588eef
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 09:29:56 +0100

    javaDoc for CriminalRecordTest

--- src/test/java/ba/unsa/etf/rpr/domain/CriminalRecordTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/CriminalRecordTest.java
@@ -6,29 +6,51 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Test for domain class Criminal record
+ */
 public class CriminalRecordTest {
+    /**
+     * Attributes
+     */
     Criminal criminal = new Criminal(1, "Criminal", "Criminal", "2705996175015", LocalDate.of(1996,05,27));
     CriminalRecord criminalRecord = new CriminalRecord(9, "someDescription", "Stockholm", LocalDate.of(2022,05,23), "someCode", criminal);
 
+    /**
+     * Test that tests getter for attribute place
+     */
     @Test
     public void getPlaceTest() {
         assertEquals("Stockholm", criminalRecord.getPlace());
     }
 
+    /**
+     * Test that tests getter for attribute description
+     */
     @Test
     public void getDescriptionTest() {
         assertEquals("someDescription", criminalRecord.getDescription());
     }
 
+    /**
+     * Test that tests getter for attribute code
+     */
     @Test
     public void getCodeTest() {
         assertEquals("someCode", criminalRecord.getCode());
     }
 
+    /**
+     * Test that tests getter for attribute date
+     */
     @Test
     public void getDateTest() {
         assertEquals(LocalDate.of(2022, 05,23), criminalRecord.getDate());
     }
+
+    /**
+     * Test that tests getter for attribute criminal
+     */
     @Test
     public void getCriminalTest() {
         assertEquals(criminal, criminalRecord.getCriminal());
