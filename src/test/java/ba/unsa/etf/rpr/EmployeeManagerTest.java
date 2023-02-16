b069a2d7289daabdd8d67a325ec56de8cb64690b
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 10:01:04 +0100

    test for exception when password is not correct

--- src/test/java/ba/unsa/etf/rpr/EmployeeManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/EmployeeManagerTest.java
@@ -1,18 +1,34 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Employee;
+import ba.unsa.etf.rpr.exceptions.CriminalRecordsException;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EmployeeManagerTest {
     Employee employee = new Employee();
 
-    @BeforeAll
-    public void setEmployee() {
+    @BeforeEach
+    public  void setEmployee() {
         employee.setId(1);
         employee.setFirstName("Amila");
         employee.setLastName("Rovcanin");
         employee.setUsername("arovcanin1");
         employee.setEmail("arovcanin1@etf.unsa.ba");
         employee.setPassword("someBadPass");
+    }
+
+    @Test
+    public void exceptionTest() {
+        try {
+            DaoFactory.employeesDao().add(employee);
+        } catch (CriminalRecordsException e) {
+            assertEquals("Password is not correct!", e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
