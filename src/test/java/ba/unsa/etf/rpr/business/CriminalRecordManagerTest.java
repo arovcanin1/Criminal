8b90f975d714da312dd32b5115e9d3f43225e414
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 10:26:10 +0100

    setting criminal record for testing

--- src/test/java/ba/unsa/etf/rpr/business/CriminalRecordManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/CriminalRecordManagerTest.java
@@ -1,4 +1,19 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.domain.CriminalRecord;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.time.LocalDate;
+
 public class CriminalRecordManagerTest {
+    CriminalRecord criminalRecord = new CriminalRecord();
+
+    @BeforeEach
+    public void setCriminalRecordTest() {
+        criminalRecord.setId(12);
+        criminalRecord.setPlace("Ljubljana");
+        criminalRecord.setCode("codecode");
+        criminalRecord.setDescription("Kill");
+        criminalRecord.setDate(LocalDate.of(2025, 06,03));
+    }
 }
