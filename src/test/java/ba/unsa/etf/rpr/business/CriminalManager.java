a75a0de665fb3557a1c63c28416f608d273351b7
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 10:14:03 +0100

    setting criminal for testing

--- src/test/java/ba/unsa/etf/rpr/business/CriminalManager.java
+++ src/test/java/ba/unsa/etf/rpr/business/CriminalManager.java
@@ -1,4 +1,20 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.domain.Criminal;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.time.LocalDate;
+
 public class CriminalManager {
+
+    Criminal criminal = new Criminal();
+
+    @BeforeEach
+    public void setCriminalTest() {
+        criminal.setId(1);
+        criminal.setFirstName("");
+        criminal.setLastName("");
+        criminal.setJmbg("2705996175015");
+        criminal.setBirthDate(LocalDate.of(1996,05,27));
+    }
 }
