7a9ec0b72d9f9c6dcba8be8d7e048de9deb6d0ad
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 10:27:49 +0100

    test for CriminalRecordManager exception

--- src/test/java/ba/unsa/etf/rpr/business/CriminalRecordManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/CriminalRecordManagerTest.java
@@ -1,9 +1,14 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.CriminalRecord;
+import ba.unsa.etf.rpr.exceptions.CriminalRecordsException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CriminalRecordManagerTest {
     CriminalRecord criminalRecord = new CriminalRecord();
@@ -15,5 +20,14 @@ public class CriminalRecordManagerTest {
         criminalRecord.setCode("codecode");
         criminalRecord.setDescription("Kill");
         criminalRecord.setDate(LocalDate.of(2025, 06,03));
+    }
+
+    @Test
+    public void exceptionDateTest() {
+        try {
+            DaoFactory.criminalRecordsDao().add(criminalRecord);
+        } catch (CriminalRecordsException e) {
+            assertEquals("Date is not correct", e.getMessage());
+        }
     }
 }
