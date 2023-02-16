d1004296248e83008606213cb27955098d8fb37d
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 09:01:41 +0100

    javaDoc for class EmployeeTest

--- src/test/java/ba/unsa/etf/rpr/domain/EmployeeTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/EmployeeTest.java
@@ -5,10 +5,18 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Tests for domain class Employee
+ */
 public class EmployeeTest {
-
+    /**
+     * Attribute new Employee
+     */
     private Employee employee = new Employee();
 
+    /**
+     * Setting Employee for testing
+     */
     @BeforeEach
     public void setEmployee() {
         employee.setId(1);
@@ -19,26 +27,41 @@ public class EmployeeTest {
         employee.setPassword("amilaamila1-");
     }
 
+    /**
+     * Test which tests getter for attribute firstName
+     */
     @Test
     void getFirstName() {
         assertEquals("Amila", employee.getFirstName());
     }
 
+    /**
+     * Test which tests getter for attribute lastName
+     */
     @Test
     void getLastName() {
         assertEquals("Rovcanin", employee.getLastName());
     }
 
+    /**
+     * Test which tests getter for attribute username
+     */
     @Test
     void getUsername() {
         assertEquals("arovcanin1", employee.getUsername());
     }
 
+    /**
+     * Test which tests getter for attribute email
+     */
     @Test
     void getEmail() {
         assertEquals("arovcanin1@etf.unsa.ba", employee.getEmail());
     }
 
+    /**
+     * Test which tests getter for attribute password
+     */
     @Test
     void getPassword() {
         assertEquals("amilaamila1-", employee.getPassword());
