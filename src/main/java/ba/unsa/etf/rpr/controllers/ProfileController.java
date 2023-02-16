c83bd02ac17ede065e23eb283f36fd7ee55ab903
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 05:48:09 +0100

    setting controller for update

--- src/main/java/ba/unsa/etf/rpr/controllers/ProfileController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ProfileController.java
@@ -1,10 +1,22 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.Dao;
+import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Employee;
+import ba.unsa.etf.rpr.exceptions.CriminalRecordsException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class ProfileController {
     Employee employee = new Employee();
@@ -29,5 +41,34 @@ public class ProfileController {
         emailFld.setText(employee.getEmail());
         usernameFld.setText(employee.getUsername());
         passwordFld.setText(employee.getPassword());
+
+        try {
+            DaoFactory.employeesDao().update(employee);
+        } catch (CriminalRecordsException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+            loader.setController(new LoginController());
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("CR Criminals");
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void update(ActionEvent event) {
+        employee.setFirstName(firstNameFld.getText());
+        employee.setLastName(lastNameFld.getText());
+        employee.setEmail(emailFld.getText());
+        employee.setEmail(emailFld.getText());
+        employee.setUsername(usernameFld.getText());
+        employee.setPassword(passwordFld.getText());
     }
 }
