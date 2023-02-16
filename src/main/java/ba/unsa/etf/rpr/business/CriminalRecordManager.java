da1d6bd63aad9bcef2fcd64a299fcb97b45dcd26
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 08:05:14 +0100

    javaDoc for CriminalRecordManager

--- src/main/java/ba/unsa/etf/rpr/business/CriminalRecordManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/CriminalRecordManager.java
@@ -5,8 +5,18 @@ import ba.unsa.etf.rpr.domain.Criminal;
 import ba.unsa.etf.rpr.domain.CriminalRecord;
 import ba.unsa.etf.rpr.exceptions.CriminalRecordsException;
 
+/**
+ * Class that is business layer for CriminalRecord
+ * This class contains methods for checking all actions connected with adding new record
+ */
 public class CriminalRecordManager {
 
+    /**
+     * Method that adds new record only if chosen date is correct
+     * @param record
+     * @return
+     * @throws CriminalRecordsException
+     */
     public static Criminal add(CriminalRecord record) throws CriminalRecordsException {
         try {
             return DaoFactory.criminalRecordsDao().add(record).getCriminal();
