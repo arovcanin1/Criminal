62343db311c3bd3d25373d6623540eba0eea81f3
Amila Rovčanin <73299629+arovcanin1@users.noreply.github.com>
2023-02-16 06:57:40 +0100

    javaDoc for exception

--- src/main/java/ba/unsa/etf/rpr/exceptions/CriminalRecordsException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/CriminalRecordsException.java
@@ -1,5 +1,8 @@
 package ba.unsa.etf.rpr.exceptions;
 
+/**
+ * User defined Exception - CriminalRecordsException
+ */
 public class CriminalRecordsException extends Exception {
     public CriminalRecordsException(String message, Exception reason) {
         super(message, reason);
